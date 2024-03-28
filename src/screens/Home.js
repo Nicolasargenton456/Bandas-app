5773de4bcfcf890e5f7208c5d312b679f89a499c
Nicolasargenton456 <ds.46276553808@gmail.com>
2024-03-28 11:28:16 -0300

    banda

---
+++ src/screens/Home.js
@@ -0,0 +1,42 @@
+
+import React, { useState } from "react";
+import { View, Text, StyleSheet, StatusBar} from "react-native";
+import MusicItem from "../components/MusicItem";
+
+export default function Home({navigation}) {
+  const [currentPlaying, setCurrentPlaying]= useState(null);
+  const item ={
+      id:1,
+      title:"Highway to hell",
+      group: "AC/DC",
+      album_image: "https://www.acdcbrasil.net/wp-content/uploads/capa-acdc-highway-to-hell.jpg",
+  };
+
+  return (
+    <View style={ styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="#121212"/>
+      <Text style={styles.title}>Minhas Músicas</Text>
+      <MusicItem 
+      isPlaying={()=> currentPlaying == item.id} 
+      music={item} 
+      navigation={navigation} 
+      onPlayPause={() => {}} />
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create ({
+    container:{
+      flex:1,
+      backgroundColor:"#121212",
+      paddingTop:16,
+    },
+      title:{
+        fontSize:36,
+        fontWeight:"bold",
+        color:"#fff",
+        marginTop:20,
+        marginLeft:20,
+      },
+})
